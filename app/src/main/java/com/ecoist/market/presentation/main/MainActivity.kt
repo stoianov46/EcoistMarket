54a002b45ce0130829994a348a48d44463ed2ec4
stoianov46 <kirillstoainov46@gmail.com>
2020-09-26 15:24:07 +0300

    Add: AppLogger.kt

--- app/src/main/java/com/ecoist/market/presentation/main/MainActivity.kt
+++ app/src/main/java/com/ecoist/market/presentation/main/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.ecoist.market.R
 import com.ecoist.market.domain.analytics.AnalyticsLogger
+import com.ecoist.market.domain.analytics.AppLogger
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,15 +29,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppLogger.debug(TAG, "onCreate()")
         setContentView(R.layout.activity_main)
         onBackPressedDispatcher.addCallback(backPressCallback)
     }
 
     private fun stackFragment(fragment: Fragment) {
+        AppLogger.debug(TAG, "stackFragment(fragment = ${fragment::class.java.simpleName})")
         this.stackFragment(fragment = fragment, stackName = fragment::class.java.simpleName)
     }
 
     private fun stackFragment(fragment: Fragment, stackName: String) {
+        AppLogger.debug(
+            TAG,
+            "stackFragment(fragment = ${fragment::class.java.simpleName}, stackName = $stackName)"
+        )
+
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.add(fragmentContainerViewId, fragment)
         fragmentTransaction.addToBackStack(stackName)
@@ -44,6 +52,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun clearStackFragment() {
+        AppLogger.debug(TAG, "clearStackFragment()")
         try {
             supportFragmentManager.popBackStackImmediate()
         } catch (throwable: Throwable) {
@@ -52,6 +61,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun clearStackFragment(stackName: String) {
+        AppLogger.debug(TAG, "clearStackFragment(stackName = $stackName)")
         try {
             supportFragmentManager.popBackStackImmediate(
                 stackName,
