559794d8b4b6510e94cf3369cdc54e416f8d59e8
Kirill Stoianov <kirill.stoianov@appflame.com>
2020-10-02 18:04:34 +0300

    add TODO

--- app/src/main/java/com/ecoist/market/presentation/category/CategoryFragment.kt
+++ app/src/main/java/com/ecoist/market/presentation/category/CategoryFragment.kt
@@ -1,9 +1,14 @@
 package com.ecoist.market.presentation.category
 
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 
 /**
  * Created by Kirill Stoianov on 26/09/2020.
  */
-class CategoryFragment:Fragment() {
+class CategoryFragment : Fragment() {
+
+    private val args: CategoryFragmentArgs by navArgs()
+
+    // TODO ADD IMPLEMENTATION WITH USING CategoryListAdapter
 }
