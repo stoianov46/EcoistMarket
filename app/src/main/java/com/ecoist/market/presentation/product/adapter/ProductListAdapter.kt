4c6faaa26968c44c1120b600e9b9021f6ef9b45e
Bulldog2740 <egorwhu@gmail.com>
2020-11-30 12:45:17 +0200

    try yo make glide

--- app/src/main/java/com/ecoist/market/presentation/product/adapter/ProductListAdapter.kt
+++ app/src/main/java/com/ecoist/market/presentation/product/adapter/ProductListAdapter.kt
@@ -1,12 +1,16 @@
 package com.ecoist.market.presentation.product.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
 import com.ecoist.market.R
 import com.ecoist.market.data.model.Product
 
@@ -35,15 +39,20 @@ class ProductListAdapter(
     }
 
     override fun onBindViewHolder(holder: ProductVh, position: Int) {
-        val item = getItem(position)
+        val item =
+            getItem(position)
         if (item != null) holder.bind(item, listener)
     }
 
     class ProductVh(view: View) : RecyclerView.ViewHolder(view) {
         private var tvProductName: TextView? = view.findViewById(R.id.tvProductName)
-
+        private var tvProductTextView: TextView? = view.findViewById(R.id.tvProductText)
+        private val myImageView : ImageView? = itemView.findViewById(R.id.tvImageView)
+        var url="https://ecoist.com.ua/salemod_img/akkumuljatornaja-batareja-alva-6fm100agm-12v-100ah.htm"
         fun bind(product: Product, listener: Listener) {
             tvProductName?.text = product.name
+            tvProductTextView?.text = product.descriptionFull
+            myImageView?.context?.let { Glide.with(it).load(url).into(myImageView) }
             itemView.setOnClickListener { listener.onClick(product) }
         }
     }
